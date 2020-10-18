b7b75056166aa0aa562fdb22f6902494469530da
Arslan majeed <arslanmajeed270@gmail.com>
2020-10-19 01:23:35 +0500

    resolving pageAction

--- house-up/house-up/src/store/actions/pageActions.js
+++ house-up/house-up/src/store/actions/pageActions.js
@@ -10,7 +10,6 @@ import {
 } from './actionTypes';
 
 let backendServerURL = process.env.REACT_APP_API_URL;
-// let routesPrefix = '/;
    
 
 
@@ -36,10 +35,10 @@ export const clearErrors = () => {
 export const getIndexPageData = () => dispatch => {
     axios
     .post(
-		// backendServerURL+'/home',
-		// {
-		// 	"userTypeId": 1
-		// }
+		backendServerURL+'/home',
+		{
+			"userTypeId": 1
+		}
     )
     .then(res => {
 		console.log('checking Home page data' , res);
