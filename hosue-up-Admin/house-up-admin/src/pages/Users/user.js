71a076a4f2b3eaff692439e30e3c4b945e9c5358
Saad Amin <61314721+mSaadAmin@users.noreply.github.com>
2020-11-29 12:49:52 +0500

    Admin Panel fixes

--- hosue-up-Admin/house-up-admin/src/pages/Users/user.js
+++ hosue-up-Admin/house-up-admin/src/pages/Users/user.js
@@ -48,29 +48,68 @@ class user extends Component {
                     <div className="row">
                     {usersData && usersData.length ? 
                       usersData.map( (data, index) =>
-                      <div key={index} className="col-lg-12">
-                        <Link to={`/single-User-${data && data.userId && data.userId}`} className="message card px-5 py-3 mb-4  no-anchor-style">
-                          <div className="row">
-                            <div className="col-lg-3 d-flex align-items-center flex-column flex-lg-row text-center text-md-left">
-                              {this.dateHandler(data.createDate)}
-                              <img src={data.profilePictureUrl ? data.profilePictureUrl : "assets/img/demo.png"} alt="..." style={{maxWidth: '3rem' , maxHeight:'3rem' , backgroundColor:'#008CF8'}} className="rounded-circle mx-3 my-2 my-lg-0" />
-                                <h6 className="mb-0">{data.firstName} {data.lastName}</h6>
-                            </div>
-                            <div className="col-lg-3 d-flex align-items-center flex-column flex-lg-row text-center text-md-left">
-                              <h6 className="mb-0">{data.emailAddress}</h6>
-                            </div>
-                            <div className="col-lg-2 d-flex align-items-center flex-column flex-lg-row text-center text-md-left">
-                              <h6 className="mb-0">@{data.userName}</h6>
-                            </div>
-                            <div className="col-lg-2 d-flex align-items-center flex-column flex-lg-row text-center text-md-left">
-                              <h6 className="mb-0">{data.msisdn}</h6>
-                            </div>
-                            <div className="col-lg-2 d-flex align-items-center flex-column flex-lg-row text-center text-md-left">
-                              <div className="bg-gray-100 roundy px-4 py-1 mr-0 mr-lg-3 mt-2 mt-lg-0 text-dark exclode">{data.userStatusDesc}</div>
-                            </div>                                       
-                          </div></Link></div>
-                      ) 
-                      : ''
+                          <div key={index} className="col-lg-12 message card px-5 py-3 mb-4">
+                            <div className="row">
+                              <div className="col-md-9">
+                              <Link to={`/single-user-${data && data.userId && data.userId}`} className="no-anchor-style" >
+                                <div className="row">
+                                <div className="col-lg-3 d-flex align-items-center flex-column flex-lg-row text-center text-md-left">
+                                  {this.dateHandler(data.createDate)}
+                                  <img src={data.profilePictureUrl ? data.profilePictureUrl : "assets/img/demo.png"} 
+                                  alt="Profile" style={{maxWidth: '48px', maxHeight:'48px' , backgroundColor:'#008CF8'}} 
+                                  className="rounded-circle mx-3 my-2 my-lg-0" />
+                                  <h6 className="mb-0">{data.firstName} {data.lastName}</h6>
+                              </div>
+                              
+                              <div className="col-lg-3 d-flex align-items-center flex-column flex-lg-row text-center text-md-left">
+                                <h6 className="mb-0">@{data.userName}</h6>
+                              </div>
+                              <div className="col-lg-3 d-flex align-items-center flex-column flex-lg-row text-center text-md-left">
+                                <h6 className="mb-0">{data.msisdn}</h6>
+                              </div>
+                              <div className="col-lg-3 d-flex align-items-center flex-column flex-lg-row text-center text-md-left">
+                                <h6 className="mb-0">{data.emailAddress}</h6>
+                              </div>
+                              </div>
+                              </Link>
+                              </div>
+                              <div className="col-lg-3 d-flex align-items-center flex-column flex-lg-row text-center text-md-left">
+                                <div className="row">
+                                  {
+                                    data.userStatusDesc === "In Review" ?
+                                    <>
+                                      <div className="col-md-6">
+                                      <button className="btn btn-success status-btn" 
+                                        onClick={()=>this.updateUserState("Active" , data.userId)}>
+                                          APPROVE
+                                      </button>
+                                      </div>
+                                      <div className="col-md-6">
+                                        <button className="btn btn-danger status-btn" 
+                                          onClick={()=>this.updateUserState("Rejected" , data.userId)}>
+                                            REJECT
+                                        </button>
+                                      </div>
+                                    </>
+                                    :
+                                    <>
+                                      <div className="col-md-6" />
+                                      <div className="col-md-6">
+                                        <button 
+                                          className={`btn  ${data.userStatusDesc === "Approved" || data.userStatusDesc === "Active" ? "btn-success" : "btn-danger"} status-btn`}
+                                        >
+                                          {data.userStatusDesc === "Approved" || data.userStatusDesc === "Active" ? "APPROVED" : "REJECTED"}
+                                        </button>
+                                      </div>
+                                    </>
+                                  }
+                                  
+                                </div>
+                              </div>
+                           </div>
+                          </div> 
+                        )
+                        : ''
                     }            
 
                       {/* <div className="col-lg-12"><Link to="single-user" className="message card px-5 py-3 mb-4  no-anchor-style">
---
+++ hosue-up-Admin/house-up-admin/src/pages/Users/userPostsList.js
@@ -0,0 +1,136 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+
+import { connect } from 'react-redux';
+import * as actions from '../../store/actions/index';
+
+class userPostsList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          id: null,
+          singleVendorsPropertiesData:{}
+        };
+      }
+      static getDerivedStateFromProps(props, state) {
+  
+        let userPage = props.userPage;
+    
+        let stateChanged = false;
+        let changedState = {};
+    
+        if (
+            userPage &&
+            JSON.stringify(state.singleVendorsPropertiesData) !==
+            JSON.stringify(userPage.singleVendorsPropertiesData)
+            ) {
+                changedState.singleVendorsPropertiesData =
+                userPage.singleVendorsPropertiesData;
+                stateChanged = true;
+            }
+    
+        if(stateChanged){
+          return changedState;
+        }
+        return null;
+    
+      }
+    
+      componentDidMount() {
+      console.log('hello')
+      const id = this.props.id
+        this.setState({
+          id: id
+        });
+        const data = {
+                offset: '0',
+                lat: '43.787083',
+                userId: id,
+                channel: 'web',
+                lng: '-79.497369',
+                limit: '10',
+            };
+        this.props.onGetSingleVendorsPropertyData(data);
+      }
+    render() {
+        const {singleVendorsPropertiesData} = this.state;
+        return (
+            <React.Fragment>
+                 {/* {singleVendorsPropertiesData &&
+                      singleVendorsPropertiesData.length ? (
+                        <h2 className='pxp-section-h2 mt-100'>
+                          Listings by {singleVendorData && singleVendorData.firstName}{' '}
+                          {singleVendorData && singleVendorData.lastName}{' '}
+                        </h2>
+                      ) : (
+                        ''
+                      )} */}
+                      <div className='row mt-4 mt-md-5'>
+                        {singleVendorsPropertiesData &&
+                        singleVendorsPropertiesData.length
+                          ? singleVendorsPropertiesData.map((data, idx) =>
+                              data && data.category === 'Post' ? (
+                                <>
+                                  <div
+                                    key={idx}
+                                    className='col-sm-12 col-md-6 col-lg-4'
+                                  >
+                                    <Link
+                                      to="#"
+                                      className='pxp-prop-card-1 rounded-lg mb-4'
+                                    >
+                                { data &&
+                                    data.object.postImages &&
+                                    data.object.postImages.length
+                                        ? 
+                                        <>
+                                        <div
+                                            className='pxp-prop-card-1-fig pxp-cover'
+                                            style={{
+                                            backgroundImage: `url(${
+                                                data.object.postImages[0] &&
+                                                data.object.postImages[0].imageURL 
+                                            })`,
+                                            }}
+                                        />
+                                        <div className='pxp-prop-card-1-gradient pxp-animate' />
+                                        <div className='pxp-prop-card-1-details'>
+                                            <div className='pxp-prop-card-1-details-title'>
+                                                {data && data.object && data.object.postText}
+                                            </div>
+                                       </div>
+                                       </>
+                                       :
+                                       <div className='pxp-prop-card-1-details'>
+                                            <div className='pxp-prop-card-1-details-title' style={{color:'black'}}>
+                                                {data && data.object && data.object.postText}
+                                            </div>
+                                        </div>
+                                }
+                                    </Link>
+                                  </div>
+                                </>
+                              ) : (
+                                ''
+                              )
+                            )
+                          : ''}
+                            </div>
+            </React.Fragment>
+        )
+    }
+}
+const mapStateToProps = state => {
+    return {
+      userPage: state.userPage
+    }
+  };
+  
+  const mapDispatchToProps = dispatch => {
+    return {
+        onGetSingleVendorsPropertyData: (userData) => dispatch(actions.getSingleVendorsPropertyData(userData))
+  
+    }
+  };
+   
+  export default connect(mapStateToProps, mapDispatchToProps)(userPostsList);
